d2e40da7c069cf6db81abac11fb23089eed8f52e
youyj <985202800@qq.com>
2018-08-04 12:25:21 +0800

    11111

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+<div>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <Test />
+ </div>
+</div>
+ 
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import Test from './components/Test.vue'
+
+
+export default {
+  name: 'app',
+  components: {
+    // HelloWorld,
+    Test
+  }
+
+}
+
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Test.vue
@@ -0,0 +1,74 @@
+<template>
+
+<el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
+    <el-form-item label="用户名" prop="userName">
+        <el-input v-model="ruleForm.userName"></el-input>
+    </el-form-item>
+    <el-form-item label="密码" prop="passWord">
+        <el-input type="password" v-model="ruleForm.passWord" auto-complete="off"></el-input>
+    </el-form-item>
+    <!-- <el-form-item label="确认密码" prop="checkPass">
+        <el-input type="password" v-model="ruleForm.checkPass" auto-complete="off"></el-input>
+    </el-form-item> -->
+
+    <el-form-item>
+        <el-button type="primary" @click="submitForm('ruleForm')">登录</el-button>
+        <!-- <el-button type="primary" @click="submitForm('ruleForm')">注册</el-button> -->
+        <el-button @click="resetForm('ruleForm')">重置</el-button>
+  </el-form-item>
+</el-form>
+
+
+</template>
+
+<script>
+export default {
+    name: 'Test',
+    data() {
+      return {
+        ruleForm: {
+          userName: 'aa',
+          passWord: ''   
+        },
+        rules: {
+          userName: [
+            { required: true, message: '请输入用户名', trigger: 'blur' },
+            { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
+          ],
+          passWord: [
+            { required: true, message: '请输入', trigger: 'blur' },
+            { min: 6,  message: '长度在 6 个字符以上', trigger: 'blur' }
+          ]
+        }
+          
+      }
+    },
+    methods: {
+      submitForm(formName) {
+        this.$refs[formName].validate((valid) => {
+          if (valid) {
+            // alert('submit!');
+            // console.log(this.ruleForm)
+          } else {
+            console.log('error submit!!');
+            return false;
+          }
+        });
+      },
+      resetForm(formName) {
+        this.$refs[formName].resetFields();
+      }
+    }
+
+}
+</script>
+
+<style>
+.demo-ruleForm{
+  width:400px;
+  margin:0 auto;
+}
+
+</style>
+
+
